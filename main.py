45f01aded9500d8db9ce1d1af742a2a8cac48915
AT <shuffletick@gmail.com>
2024-01-07 18:43:47 -0700

    get first command work; mvp delivered

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!./.venv/bin/python3
 import os
 import random
+import requests
 
 import discord
 from discord.ext import commands
+
+from pprint import pprint
 
 api_token = os.environ["DISCORD_KEY"]
 # This example requires the 'members' and 'message_content' privileged intents to function.
@@ -21,6 +24,20 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", description=description, intents=intents)
 
 
+target_regions = [
+    "Providence",
+    "Catch",
+    "Tenerifis",
+    "Scalding Pass",
+    "Querious",
+    "Wicked Creek",
+    "Deteroid",
+    "Esoteria",
+    "Etherium Reach",
+    "Pure Blind",
+]
+
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
@@ -28,41 +45,27 @@ async def on_ready():
 
 
 @bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
+async def connections(ctx):
+    """Lists connnections that we can roam from"""
+    thera_connections = requests.get('https://api.eve-scout.com/v2/public/signatures')
+    thera_data = thera_connections.json()
+    for system in thera_data:
+        if system["in_region_name"] in target_regions and system["out_system_name"] == "Thera":
 
-
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split("d"))
-    except Exception:
-        await ctx.send("Format has to be in NdN!")
-        return
-
-    result = ", ".join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-
-@bot.command(description="For when you wanna settle the score some other way")
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
-
-
-@bot.command()
-async def repeat(ctx, times: int, content="repeating..."):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
-
-
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send(f"{member.name} joined {discord.utils.format_dt(member.joined_at)}")
+            region = system["in_region_name"]
+            life = system["remaining_hours"]
+            out_sig = system["out_signature"]
+            system_name = system["in_system_name"]
+            pprint(system["in_region_name"])
+            pprint(system["remaining_hours"])
+            pprint(system["out_signature"])
+            pprint(system["in_system_name"])
+            await ctx.send(f"Region: {region}")
+            await ctx.send(f"System: {system_name}")
+            await ctx.send(f"Out Sig: {out_sig}")
+            await ctx.send(f"Life remaining*: {life}")
+            await ctx.send("------")
+        # await ctx.send("""We should roam...\n""")
 
 
 @bot.group()
